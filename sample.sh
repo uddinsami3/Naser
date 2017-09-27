55d3ade4588ee619266de977c86739049f9b813a
Uddin <Syed.Uddin@mastertest.com>
2017-09-26 22:39:57 -0500

    sample demo delete

--- sample.sh
+++ sample.sh
@@ -2,5 +2,5 @@
 echo $PWD
 for x in "$(git log --name-only | grep ".tst")"; do
 	echo "$x"
-	curl --insecure --request GET --url "http://10.157.138.180:7820/lisa-invoke/runTest?testCasePath="$x"&stagingDocPath=Accel/StagingDocs/Run1User1Cycle.stg"
+	curl --insecure --request GET --url "http://10.157.138.180:7820/lisa-invoke/runTest?testCasePath="$x"&stagingDocPath=AIMWS/StagingDocs/Run1User1Cycle.stg"
 done
